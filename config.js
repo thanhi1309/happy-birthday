e52182f2d567c1932a4879c7152884d3e54ee3c6
Hien Nguyen <nguyenhien.py.1996@gmail.com>
2023-09-12 14:29:10 +0700

    first commmit

---
+++ config.js
@@ -0,0 +1,29 @@
+var config = {
+    // Cố gắng không vượt quá 15 từ mỗi câu, nếu không hiệu ứng hiển thị có thể không tốt lắm
+    texts: [
+        "Chúc mừng sinh nhật",      
+        "Thanh Nhi",  
+        "Hôm nay là sinh nhật của Nhi",
+        "Chúc Thanh Nhi luôn...",
+        "Xinh đẹp rạng ngời",
+        "Một đời hạnh phúc",
+        "Mạnh khỏe",
+        "An nhiên ",
+        "Và bình an Nhi nhé!",
+        "Thanhi~~",
+    ],
+
+    imgs: {
+        "Thanh Nhi": "./imgs/thanhi.jpg",
+    },
+    desc: {
+        turn_on: "Start",
+        play: "Play",
+        // bannar_coming: "màu sắc",
+        // balloons_flying: "Có vẻ như thiếu một cái gì đó",
+        cake_fadein: "Cake",
+        light_candle: "Light Candle",
+        wish_message: "Happy Birthday",
+        story: "Message",
+    }
+};
